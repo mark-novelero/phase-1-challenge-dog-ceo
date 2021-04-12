3ca969b1235fc93cfe4b17b24c00146cdae70a74
mark novelero <mark.novelero@gmail.com>
2021-04-12 14:06:26 -0400

    first

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', e => {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(res => res.json())
+    .then(e => {
+        let dogImage = document.querySelector('div#dog-image-container')
+        let picture = document.createElement('img')
+        e.message.forEach(e => {picture.src = e.value
+        dogImage.append(picture)})
+        
+    })
+})
